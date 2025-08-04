86152bdd77f0bf39b111101d33e7543b7d8db15e
cilio <cilio.chalhoub@f8conseil.com>
2025-08-04 22:01:07 +0200

    update

--- terraform/main.tf
+++ terraform/main.tf
@@ -53,6 +53,19 @@ resource "azurerm_network_security_group" "nsg" {
     source_address_prefix      = "*"
     destination_address_prefix = "*"
   }
+  
+  security_rule {
+    name                       = "Allow-NodeExporter"
+    priority                   = 102
+    direction                  = "Inbound"
+    access                     = "Allow"
+    protocol                   = "Tcp"
+    source_port_range          = "*"
+    destination_port_range     = "9100"
+    source_address_prefix      = "*"
+    destination_address_prefix = "*"
+  }
+
 }
 
 resource "azurerm_public_ip" "public_ip" {
